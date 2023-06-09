e6cf8328c121ee9e07be0db044d43f012b6d5416
mocheng <18830279823@163.com>
2023-06-09 14:43:02 +0800

    备案信息

---
+++ src/components/BaseSideFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div wfull bg-gray:80 h10 rounded-2 flex items-center justify-center pr-2 box-border>
+    <a class="test" decoration-none relative color-white font-bold opacity80 href="https://beian.miit.gov.cn/"
+      target="_blank">备案号：京ICP备2023013429号-1</a>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.test {
+  @include base-text-shadow(black, "备案号：京ICP备2023013429号-1")
+}
+</style>
